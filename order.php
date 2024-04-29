ad3ec71d8aa662cd029987c8deb58df6ac1daea4
Ahmed58765 <166318891+Ahmed58765@users.noreply.github.com>
2024-04-29 20:13:21 +0400

    fv

--- order.php
+++ order.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="wdith= device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://fonts.cdnfonts.com/css/lato" rel="stylesheet">
     <title>EdgyTech</title>
@@ -23,29 +23,38 @@
         </div>
     </section>
 
-    <section id="orders">
-        <h2>Your Orders</h2>
-        <table>
-            <thead>
-                <tr>
-                    <th>Order ID</th>
-                    <th>Status</th>
-                    <th>Date</th>
-                    <th>Total Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!-- Add rows dynamically here using PHP or JavaScript -->
-                <tr>
-                    <td>1</td>
-                    <td>Shipped</td>
-                    <td>2024-04-29</td>
-                    <td>$120.00</td>
-                </tr>
-                <!-- Add more rows as needed -->
-            </tbody>
-        </table>
+    <section class="checkout">
+        
+        <div class="ibox-content">
+            <div class="table-responsive">
+                <table class="table shoping-cart-table">
+                    <tbody>
+                        <tr>
+                            <td width="90">
+                                <!-- Image Here -->
+                                <img src="product_image.jpg" alt="Product Image" style="width: 80px;">
+                            </td>
+                            <td class="desc">
+                                <!-- Product Name and Price -->
+                                <h3><a href="#" class="text-navy">Desktop publishing software</a></h3>
+                                <div class="m-t-sm">300 AED</div>
+                                <!-- Remove item link -->
+                                <div class="m-t-sm">
+                                    <h3>Order Date</h3>
+                                    <h3>Order Status</h3>
+                                </div>
+                            </td>
+                            <td width="80">
+                                <!-- Quantity input -->
+                                <p>Quantity:</p>
+                                <input type="text" class="form-control" placeholder="1">
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
     </section>
 </body>
 </html>
-
